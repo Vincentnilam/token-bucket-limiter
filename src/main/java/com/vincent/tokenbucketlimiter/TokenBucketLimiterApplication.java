5627740eef45bc2439abaf6c3e8a0fe0d609e404
Vincent Nilam <vincent.nilam@hotmail.com>
2026-07-06 21:31:59 +1000

    chore: initial commit springbooot v4.1.0

---
+++ src/main/java/com/vincent/tokenbucketlimiter/TokenBucketLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.tokenbucketlimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenBucketLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenBucketLimiterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincent/tokenbucketlimiter/TokenBucketLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.tokenbucketlimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenBucketLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
